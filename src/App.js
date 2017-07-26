cbb8488f8814b362767b288386bea44249657187
Roy Veshovda <roy@veshovda.no>
2017-07-26 12:02:56 +0200

    feat: List Books
    List books in separate component, and filter for each section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-//import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
@@ -10,11 +10,17 @@ class BooksApp extends React.Component {
     books: []
   }
 
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+    })
+  }
+
   render() {
     return (
       <div className="app">
         <Route exact path="/" render={() => (
-          <ListBooks />
+          <ListBooks books={this.state.books}/>
         )} />
         <Route path="/search" render={() => (
           <SearchBooks />
